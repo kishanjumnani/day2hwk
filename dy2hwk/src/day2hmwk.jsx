eb89543486d65a4a4dd34cecc781164443ad765e
kishanjumnani <kishanimpjumnani@gmail.com>
2024-05-29 21:59:58 +0530

    some

---
+++ dy2hwk/src/day2hmwk.jsx
@@ -0,0 +1,29 @@
+ function shwdata(){
+    return(
+        <>
+        <div className="container">
+            <h1 className="heading">Topics covered</h1>
+            <p>The following is a list of all the topics we cover in the MDN learning area.</p>
+            <ul>
+                <li>
+            <a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web">Getting started with the web</a>
+            <p>Provides a practical introduction to web development for complete beginners.</p>
+            </li>
+          
+                <li>
+            <a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web">HTML — Structuring the web</a>
+            <p>HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.</p>
+            </li>
+          
+                <li>
+            <a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web">CSS — Styling the web</a>
+            <p>CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.</p>
+            </li>
+          
+
+            </ul>
+        </div>
+        </>
+    )
+ }
+ export default shwdata;
